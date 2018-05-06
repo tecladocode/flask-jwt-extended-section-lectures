30d95ee14623364b7100ae6c584e14034a6e0164
Jose Salvatierra <jslvtr@gmail.com>
2018-05-06 21:47:38 +0100

    Add UserLogout to app.py.

--- 11_logging_users_out/end/app.py
+++ 11_logging_users_out/end/app.py
@@ -4,7 +4,7 @@ from flask_jwt_extended import JWTManager
 
 from db import db
 from blacklist import BLACKLIST
-from resources.user import UserRegister, User, UserLogin, TokenRefresh
+from resources.user import UserRegister, User, UserLogin, TokenRefresh, UserLogout
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
@@ -92,6 +92,7 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(User, '/user/<int:user_id>')
 api.add_resource(UserLogin, '/login')
 api.add_resource(TokenRefresh, '/refresh')
+api.add_resource(UserLogout, '/logout')
 
 if __name__ == '__main__':
     db.init_app(app)
